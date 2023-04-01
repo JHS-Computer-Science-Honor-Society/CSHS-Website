a56e0c56945e12d0bd5308966062c72f3918042f
vedantgrover <68160833+vedantgrover@users.noreply.github.com>
2023-04-01 14:51:32 -0700

    Website setup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Imports
 const express = require("express");
-
 const app = express();
 const port = 3000;
 
@@ -8,12 +7,26 @@ const port = 3000;
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + "public/css"));
 
+app.use(express.static('public'));
+app.use('/js', express.static(__dirname + "public/js"));
+
+app.use(express.static('public'));
+app.use('/images', express.static(__dirname + "public/images"));
+
 // Set Views
 app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('', (req, res) => {
     res.render('index');
+});
+
+app.get('/resources', (req, res) => {
+    res.render('resources');
+});
+
+app.get('/calendar', (req, res) => {
+    res.render('calendar');
 });
 
 // Listen on port
